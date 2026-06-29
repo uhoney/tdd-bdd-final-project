39c41cc983517158c411188665477c47f33bda3d
uhoney <t2maju6@students.oamk.fi>
2026-06-29 09:15:39 -0400

    not 95 percent herpderp

--- tests/factories.py
+++ tests/factories.py
@@ -14,9 +14,7 @@
 
 # pylint: disable=too-few-public-methods
 
-"""
-Test Factory to make fake objects for testing
-"""
+"""Test Factory to make fake objects for testing"""
 import factory
 from factory.fuzzy import FuzzyChoice, FuzzyDecimal
 from service.models import Product, Category
@@ -27,8 +25,33 @@ class ProductFactory(factory.Factory):
 
     class Meta:
         """Maps factory to data model"""
-
         model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    name = FuzzyChoice(
+        choices=[
+            "Hat",
+            "Pants",
+            "Shirt",
+            "Apple",
+            "Banana",
+            "Pots",
+            "Towels",
+            "Ford",
+            "Chevy",
+            "Hammer",
+            "Wrench"
+            ])
+
+    description = factory.Faker("text")
+    price = FuzzyDecimal(0.5, 2000.0, 2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(
+        choices=[
+            Category.UNKNOWN,
+            Category.CLOTHS,
+            Category.FOOD,
+            Category.HOUSEWARES,
+            Category.AUTOMOTIVE,
+            Category.TOOLS,
+            ])
